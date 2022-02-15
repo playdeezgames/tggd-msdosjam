bfb1d01f07ac78b63d1377318e9c3eb0ffeb3faf
ernestpazera <ernestpazera@gmail.com>
2022-02-15 04:08:28 -0600

    quitting: the game!

---
+++ TGGD_msdosjam/HideseekersOfSPLORR/MainMenu.vb
@@ -0,0 +1,20 @@
+﻿Imports Terminal.Gui
+
+Module MainMenu
+    Private done As Boolean
+    Private Sub ConfirmQuit()
+        If MessageBox.Query("Are you sure?", "Are you sure you want to quit?", "No", "Yes") = 1 Then
+            Application.RequestStop()
+            done = True
+        End If
+    End Sub
+    Sub Run()
+        done = False
+        Dim quitButton As New Button("Quit")
+        AddHandler quitButton.Clicked, AddressOf ConfirmQuit
+        Dim dlg As New Dialog("Hideseekers of SPLORR!!", quitButton)
+        While Not done
+            Application.Run(dlg)
+        End While
+    End Sub
+End Module
--- TGGD_msdosjam/HideseekersOfSPLORR/Program.vb
+++ TGGD_msdosjam/HideseekersOfSPLORR/Program.vb
@@ -3,6 +3,6 @@ Imports Terminal.Gui
 Module Program
     Sub Main(args As String())
         Application.Init()
-        MessageBox.Query("Hello, world!", "This is a Terminal.Gui application written in VB.", "Ok")
+        MainMenu.Run()
     End Sub
 End Module
