6bfe6df68f3e4dd9ee0a401dbe0b7968114fee05
ernestpazera <ernestpazera@gmail.com>
2022-02-15 16:09:22 -0600

    also dont just go back to main menu from in play menu

--- TGGD_msdosjam/HideseekersOfSPLORR/InPlay.vb
+++ TGGD_msdosjam/HideseekersOfSPLORR/InPlay.vb
@@ -14,12 +14,18 @@ Module InPlay
     Private Sub UpdateLabels()
         facingLabel.Text = $"Facing: {PlayerCharacter.Facing} "
     End Sub
+    Private Sub HandleKeyEvent(evt As View.KeyEventEventArgs)
+        If evt.KeyEvent.Key = Key.Esc Then
+            evt.Handled = Not GameMenu.Run()
+        End If
+    End Sub
     Sub Run()
         Dim abandonButton As New Button("Menu")
         AddHandler abandonButton.Clicked, AddressOf Menu
         Dim turnButton As New Button("Turn...")
         AddHandler turnButton.Clicked, AddressOf Turn
         Dim dlg As New Dialog("In Play", turnButton, abandonButton)
+        AddHandler dlg.KeyPress, AddressOf HandleKeyEvent
         dlg.Add(New Label(0, 0, $"CharacterId: {PlayerCharacter.Id}"))
         dlg.Add(New Label(0, 1, $"X: {PlayerCharacter.Location.X}"))
         dlg.Add(New Label(0, 2, $"Y: {PlayerCharacter.Location.Y}"))
