88a05f229248ff81990be8dad5993fdc4b0582ca
ernestpazera <ernestpazera@gmail.com>
2022-02-18 05:53:21 -0600

    wabbit season

---
+++ TGGD_msdosjam/HideseekersOfSPLORR.Game/CritterType.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Enum CritterType
+    Wabbit
+    Duck
+End Enum
+Module CritterTypeExtensions
+    <Extension()>
+    Function GetName(critterType As CritterType) As String
+        Select Case critterType
+            Case CritterType.Wabbit
+                Return "wabbit"
+            Case CritterType.Duck
+                Return "duck"
+            Case Else
+                Throw New NotImplementedException()
+        End Select
+    End Function
+    <Extension()>
+    Function GetDescription(critterType As CritterType) As String
+        Select Case critterType
+            Case CritterType.Wabbit
+                Return "a wascally wabbit"
+            Case CritterType.Duck
+                Return "a duck"
+            Case Else
+                Throw New NotImplementedException()
+        End Select
+    End Function
+End Module
